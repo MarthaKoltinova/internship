6cdd50b82869ea5f62a6e4aa8ce79e362dcf5eb8
MarthaKoltinova <martitakoltinova@gmail.com>
2022-03-01 21:45:48 +0300

    nothing good

--- src/main/java/PageObject/BasePage.java
+++ src/main/java/PageObject/BasePage.java
@@ -1,11 +1,14 @@
 package PageObject;
+import java.util.logging.Logger;
 
+import org.apache.log4j.Level;
 import org.openqa.selenium.WebDriver;
 
 public abstract class BasePage {
     protected WebDriver driver;
-
+    //protected static Logger log = Logger.getLogger(BasePage.class.getName());
     protected BasePage(WebDriver driver) {
+       // log.log(Level.DEBUG"");
         this.driver = driver;
     }
 
--- src/test/java/Task5/Task5.java
+++ src/test/java/Task5/Task5.java
@@ -7,20 +7,40 @@ import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.logging.Logger;
+
 public class Task5 extends BaseTest {
+   /* static Logger LOGGER;
+    private static final java.util.logging.LogManager LogManager = ;
+
+    static {
+        try (FileInputStream ins = new FileInputStream("src/main/resources/log4j.properties")) {
+            LogManager.getLogManager().readConfiguration(ins);
+        } catch (Exception ignore) {
+            ignore.printStackTrace();
+        }
+    }
+    System.setErr(new PrintStream(new File("log.txt")));
+
+    */
 
     @Parameters("searchValue")
-    @Test(priority = 1)
-    public void preconditions(String searchValue) {
+    @Test(description = "enterValue", priority = 1)
+    public void enterValue(String searchValue) {
+        //LOGGER.info("Начало теста enterValue,открываем сайт и вводим ITechart");
         get(HomePage.class)
                 .open(property.getProperty("url"))
                 .sendString(searchValue)
                 .enter();
+       // LOGGER.info("Конец теста enterValue");
     }
 
     @Parameters({"searchValue", "resultCount"})
     @Test(priority = 2)
     public void pageResults(String searchValue, String resultCount) {
+       // LOGGER.info("Начало теста pageResults,получаем результаты поиска и сравниваем их со словом ITechart");
         checkSearchResult(searchValue);
         int expectedResult = Integer.parseInt(resultCount);
         int actualResult = Integer.parseInt(get(SearchResultsPage.class).getResultCount());
@@ -30,6 +50,7 @@ public class Task5 extends BaseTest {
         get(SearchResultsPage.class)
                 .clickOnPageNumber(2);
         checkSearchResult(searchValue);
+       // LOGGER.info("Конец теста pageResults");
     }
 
     private void checkSearchResult(String searchValue) {
